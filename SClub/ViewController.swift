b83d0fa1930768ee025742f567aed8e903dfedcd
Sultan Aliev <greenstar153@gmail.com>
2020-04-20 16:11:26 +0300

    Initial Commit

---
+++ SClub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SClub
+//
+//  Created by Sultan on 20.04.2020.
+//  Copyright © 2020 com.Sultan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
