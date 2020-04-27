6fc601489fcd6041c16cf620e0076c598ddcbe8f
Sultan Aliev <greenstar153@gmail.com>
2020-04-27 15:29:32 +0300

    Added tabBar

---
+++ SClub/Controllers/TabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  TabBarController.swift
+//  SClub
+//
+//  Created by Sultan on 27.04.2020.
+//  Copyright © 2020 com.Sultan. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.selectedIndex = 2
+    }
+
+}
